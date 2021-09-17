5aef06c1c82f11d73badc9b3999391cd35d4355f
Nshiv011 <61015899+Nshiv011@users.noreply.github.com>
2021-09-18 01:45:33 +0530

    a

--- src/Atoms/Icons.jsx
+++ src/Atoms/Icons.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Icon(props){
         return(
           <>
-<svg xmlns="http://www.w3.org/2000/svg" class="h-20 w-20 p-4  shadow-sm  hover:bg-blue-400 icons-effect" viewBox="0 0 20 20">
+<svg xmlns="http://www.w3.org/2000/svg" class="h-20 w-20 p-4  shadow-sm  hover:bg-blue-400 active:bg-blue-400 icons-effect" viewBox="0 0 20 20">
 <path d={props.d}/>
 </svg>
           </>
